a9cbb501d246c527bb2f28ebdbe993c507533c97
MeetGoswami2282 <meetgoswami842@gmail.com>
2026-04-15 22:32:25 +0530

    changes

--- script.js
+++ script.js
@@ -6,3 +6,21 @@
   menuBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
   });
+
+  document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      1024: { slidesPerView: 2 }
+    }
+  });
+});
